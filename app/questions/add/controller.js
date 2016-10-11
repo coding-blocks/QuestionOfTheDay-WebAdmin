64912a250a84d407351d43f4904b48a68e01a962
Arnav Gupta <arnav@codingblocks.com>
2016-10-12 04:34:50 +0530

    saving questions is done !

--- app/questions/add/controller.js
+++ app/questions/add/controller.js
@@ -2,8 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    updateQuestion(elem) {
-      this.question = elem.innerHTML;
+    addQuestion(q, o1, o2, o3, o4) {
+      let newQuestion = this.store.createRecord('questions', {
+        question: q,
+        option1: o1,
+        option2: o2,
+        option3: o3,
+        option4: o4,
+        date: new Date()
+      });
+      newQuestion.save()
+        .then(() => {
+          // Question is saved
+          // Show modal here and do the happy dance
+        })
+        .catch((e) => {
+          console.error('save: failure', e);
+        });
     }
   }
 });
