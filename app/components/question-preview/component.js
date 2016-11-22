2915f51d3d108174d037674f8d06689941fde83a
Arnav Gupta <arnav@codingblocks.com>
2016-11-23 01:57:32 +0530

    more updates to sign in policy
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/components/question-preview/component.js
+++ app/components/question-preview/component.js
@@ -5,6 +5,7 @@ export default Ember.Component.extend({
     "use strict";
     this._super(...arguments);
     this.set('q', this.get('ques').toJSON());
+    this.set('usermode', (Parse.User.current() != null));
   },
   actions : {
     deleteQuestion(ques) {
--- app/components/top-nav/component.js
+++ app/components/top-nav/component.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   willRender : function () {
     "use strict";
+    this._super(...arguments);
     if (Parse.User.current()) {
       this.set('signedin', true);
     } else {
